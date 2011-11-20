f589affb8a82e729841ed2821687520602b6e9c2
Yamagishi Kazutoshi <ykzts@desire.sh>
2011-11-20 23:57:36 +0900

    新設したhelpers.jsをコミットするのをわすれていたので再送。

---
+++ helpers.js
@@ -0,0 +1,17 @@
+
+var settings = require('./settings');
+
+exports.get_title = function() {
+  var title = [];
+  var status = this.status;
+  var username = this.username;
+  var page = this.page;
+  if (status === 404)
+    return '404 Not found.';
+  if (page)
+    title.push(page);
+  if (username)
+    title.push(username);
+  title.push(settings.TITLE);
+  return title.join(' < ');
+};
