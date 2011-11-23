388f0b36e7f3350c9e6efc737be8a32100a9d1b0
Yamagishi Kazutoshi <ykzts@desire.sh>
2011-11-24 04:46:53 +0900

    Viewsから直接 Tumblrのライブラリーをもちいるように。

--- views/user.js
+++ views/user.js
@@ -1,24 +1,40 @@
 
 var fs = require('fs');
-var PhotosViewer = require('../lib/PhotosViewer');
+var tumblr = require('../lib/tumblr');
+var utils = require('../lib/utils');
 var views = require('./index');
 var settings = require('../settings');
-
-var pv = new PhotosViewer(settings.TUMBLR.API_KEY);
 
 module.exports = function(req, res) {
   var username = req.params.username;
   var format = req.params.format || 'html';
   var page = (req.query.page || 1) * 1;
 
-  pv.get(username, page, function(sections) {
-    if (!sections.length) {	
+  tumblr.request({
+    api_key: settings.TUMBLR.API_KEY,
+    type: 'photo',
+    limit: 20,
+    username: username,
+    page: page
+  }, function(req) {
+    req.on('data', function(json) {
+      var posts = json.response.posts || [];
+      var sections = posts.map(function(post, num) {
+        var section = utils.section_simplify(post);
+        section.num = num + 1;
+        return section;
+      });
+      if (!sections.length) {
+        views.http404(req, res);
+        return;
+      }
+      res.render('user', {
+        username: username,
+        page: page,
+        sections: sections
+      });
+    }).on('error', function(json) {
       views.http404(req, res);
-    }
-    res.render('user', {
-      username: username,
-      page: page,
-      sections: sections
     });
-  });
+  }).send();
 };
