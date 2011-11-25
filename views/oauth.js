90bff24e94459ea6aad71c7958b60ad1ef5c86bd
Yamagishi Kazutoshi <ykzts@desire.sh>
2011-11-25 19:36:41 +0900

    Viewsから直接oauthのAPIを叩くのを止めて、lib/tumblr.jsを経由させるように。

--- views/oauth.js
+++ views/oauth.js
@@ -1,38 +1,31 @@
 
-var tumblr = require('../lib/tumblr');
+var Tumblr = require('../lib/tumblr').Tumblr;
 var settings = require('../settings');
-
-const TUMBLR_AUTHORIZE_URI = 'http://www.tumblr.com/oauth/authorize';
-
-var oa = (new tumblr.Tumblr(
-  settings.TUMBLR.CONSUMER_KEY,
-  settings.TUMBLR.SECRET_KEY
-)).oa;
 
 exports.login = function(req, res) {
   var session = req.session;
+  var tum = new Tumblr(settings.TUMBLR.CONSUMER_KEY, settings.TUMBLR.SECRET_KEY);
   if (!session.oauth) {
-    oa.getOAuthRequestToken(function(err, token, token_secret) {
+    tum.oauth_callback = 'http://desire.sh/_oauth/signin';
+    tum.get_request_token(function(token, token_secret, authorize_uri) {
       session.oauth = {};
       session.oauth.token = token;
       session.oauth.token_secret = token_secret;
-      res.redirect(TUMBLR_AUTHORIZE_URI + '?oauth_token=' + token);
+      res.redirect(authorize_uri);
     });
     return;
   }
   var token = session.oauth.token;
   var token_secret = session.oauth.token_secret;
   var verifier = session.oauth.verifier = req.query.oauth_verifier;
-  oa.getOAuthAccessToken(token, token_secret, verifier,
-    function(err, access_token, access_token_secret) {
+  tum.get_access_token(token, token_secret, verifier,
+    function(access_token, access_token_secret) {
       session.oauth.access_token = access_token;
       session.oauth.access_token_secret = access_token_secret;
-      oa.get('http://api.tumblr.com/v2/user/info', access_token, access_token_secret,
-        function(err2, data) {
-          req.session.user_info = JSON.parse(data).response.user;
-          res.redirect('/_dashboard');
-        }
-      );
+      tum.get_userinfo(function(data) {
+        session.userinfo = data.response;
+        res.redirect('/_dashboard');
+      });
     }
   );
 };
